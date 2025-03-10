0ea0833971e4db9d2e3fd6efeac3ae90a36b0ab0
subhash131 <subhashnayak131@gmail.com>
2025-03-10 22:38:50 +0530

    reset app

--- components/ui/google-signin-button.tsx
+++
@@ -1,6 +0,0 @@
-import { Button } from "react-native";
-import { signInWithGoogle } from "@/auth";
-
-export default function LoginScreen() {
-  return <Button title="Sign in with Google" onPress={signInWithGoogle} />;
-}
